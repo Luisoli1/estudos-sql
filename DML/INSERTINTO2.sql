2d9eaeaaecc8cc58ee9d3fe18cb05373866f8ff4
Luis Oliveira <luishenrique.oliveira1@outlook.com>
2026-07-17 17:06:48 -0300

    Adicionando exercicios SUBLINGUAGENS DDL E DML

---
+++ DML/INSERTINTO2.sql
@@ -0,0 +1,8 @@
+USE ecommerce;
+
+INSERT INTO produtos (nome, preco, quantidade_estoque)
+VALUES 
+('Notebook pro', 4500, 10),
+('Mouse Sem Fio', 15000, 50),
+('Teclado Mecânico', 350, 0),
+('Monitor 24 Polegadas', 1200, 15);
