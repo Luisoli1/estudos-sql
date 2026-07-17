da4f2aef6b315e4364d895a54b68e97e3789dfc9
Luisoli1 <luishenrique.oliveira1@outlook.com>
2026-07-17 17:44:17 -0300

    Delete DDL/CREATEE2.sql

--- DDL/CREATEE2.sql
+++
@@ -1,9 +0,0 @@
-USE ecommerce;
-
-CREATE TABLE produtos(
-    id_produtos INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    preco DECIMAL(3,2),
-    quantidade_estoque INT NOT NULL
-);
-
